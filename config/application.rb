72b7bcad8e4f9a70f63f8ba5e03b26a158349e93
robo-hobo <robohobo-95@o2.pl>
2015-07-09 17:19:18 +0200

    PRODUCTION 5

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Facebook
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = true
   end
 end
